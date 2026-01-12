ca22a5ae02fb3d34fab70939b863509da7bcdf27
abb2k <75845401+abb2k@users.noreply.github.com>
2026-01-12 16:10:36 +0200

    flipped pinch move direction

--- src/nodes/layers/AdvancedScrollLayer.cpp
+++ src/nodes/layers/AdvancedScrollLayer.cpp
@@ -215,7 +215,7 @@ void AdvancedScrollLayer::ccTouchesMoved(CCSet* touches, CCEvent* event){
     if (d1 + d2 == 0.0f)
         result = 0.5f;
     else
-        result = d2 / (d1 + d2);
+        result = d1 / (d1 + d2);
 
     zoomToAndMove(initialScale / mult, result);
 }
